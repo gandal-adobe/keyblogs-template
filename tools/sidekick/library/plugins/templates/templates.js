e270631043861cde865c81e2a55132afff4c1db0
gandal-adobe <129903184+gandal-adobe@users.noreply.github.com>
2023-05-24 13:12:12 -0400

    Update templates.js

--- tools/sidekick/library/plugins/templates/templates.js
+++ tools/sidekick/library/plugins/templates/templates.js
@@ -137,7 +137,7 @@ function processMarkup(template, path) {
     }
   });
   // process template head to derive meta tags
-  output = output.concat('---');
+  output = output.concat('<p/>');
   output = output.concat(createMetadataTable(template.head, path));
 
   return output;
