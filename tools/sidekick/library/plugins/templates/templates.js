367a01eecf6df5cf933103d5a336dbf2d6c27fc6
gandal-adobe <129903184+gandal-adobe@users.noreply.github.com>
2023-05-25 11:42:58 -0400

    Update templates.js

--- tools/sidekick/library/plugins/templates/templates.js
+++ tools/sidekick/library/plugins/templates/templates.js
@@ -96,8 +96,7 @@ function createMetadataTable(headSection, path) {
     const headMetaTag = row.getAttributeNames()[0] === 'property' ? row.getAttribute('property') : row.getAttribute('name');
     const metaTagValue = validMetaMap[headMetaTag];
     if (metaTagValue !== undefined) {
-      const metaObj = new Object;
-      metaObj[metaTagValue] = row.getAttribute('content'); 
+      const metaObj = { 'label':metaTagValue, 'data':row.getAttribute('content') }; 
       metadataArray.push(metaObj);
     }
   });
