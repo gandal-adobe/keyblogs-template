a0475ef21bf32245e9b3c72c7f50b1135743d46c
gandal-adobe <129903184+gandal-adobe@users.noreply.github.com>
2023-05-25 12:41:12 -0400

    Update templates.js

--- tools/sidekick/library/plugins/templates/templates.js
+++ tools/sidekick/library/plugins/templates/templates.js
@@ -9,6 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
 import { createElement, createCopy } from '../utils/utils.js';
 
 const blockHeaderBGColor = '#f4cccd';
